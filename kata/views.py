ad0f8c7a6e0e95ea3ea7e9448da1c495f6182af2
huzhennan <huzhennan@kezaihui.com>
2016-03-07 19:50:35 +0800

    11111

--- kata/views.py
+++ kata/views.py
@@ -1,6 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import hashlib
+
+TOKEN = u"my token"
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("hello, world!")
+    signature = request.GET['signature']
+    timestamp = request.GET['timestamp']
+    nonce = request.GET['nonce']
+    echostr = request.GET['echostr']
+
+    sha1 = hashlib.sha1()
+    sha1.update(u''.join(sorted([TOKEN, timestamp, nonce])))
+    digest = sha1.digest()
+
+    return HttpResponse(echostr)
