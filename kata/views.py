edb47500aa52bf77cc1769127841d921a0578264
huzhennan <huzhennan@kezaihui.com>
2016-03-08 12:03:48 +0800

    version 1

--- kata/views.py
+++ kata/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 import hashlib
 
-TOKEN = u"my token"
+TOKEN = u"ZaiHuiWanSui2015"
 
 
 # Create your views here.
@@ -14,6 +14,9 @@ def index(request):
 
     sha1 = hashlib.sha1()
     sha1.update(u''.join(sorted([TOKEN, timestamp, nonce])))
-    digest = sha1.digest()
+    digest = sha1.hexdigest()
+
+    import pdb
+    pdb.set_trace()
 
     return HttpResponse(echostr)
