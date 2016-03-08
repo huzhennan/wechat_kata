e9c734eaaebd677b72ed2fda63789745ca924153
huzhennan <huzhennan@kezaihui.com>
2016-03-08 12:12:21 +0800

    use wechat sdk

--- kata/views.py
+++ kata/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 import hashlib
+from wechat_sdk import WechatConf, WechatBasic
 
 TOKEN = u"ZaiHuiWanSui2015"
+APP_ID = u"wxd1ac16e44122c49a"
+APP_SECRET = u"d3ddce902a9d3c1f2071eb25f479df33"
 
 
 # Create your views here.
@@ -12,11 +15,17 @@ def index(request):
     nonce = request.GET['nonce']
     echostr = request.GET['echostr']
 
-    sha1 = hashlib.sha1()
-    sha1.update(u''.join(sorted([TOKEN, timestamp, nonce])))
-    digest = sha1.hexdigest()
+    conf = WechatConf(
+        token=TOKEN,
+        appid=APP_ID,
+        appsecret=APP_SECRET,
+        encrypt_mode="normal"
+    )
 
-    import pdb
-    pdb.set_trace()
+    wechat = WechatBasic(conf=conf)
+    if wechat.check_signature(signature=signature, timestamp=timestamp, nonce=nonce):
+        print 'Accept'
+    else:
+        print 'Wrong'
 
     return HttpResponse(echostr)
